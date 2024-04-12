4a7141c7890f9ea32bae5a78c3deeeb6495e1717
Jiyun Di <76240530+jiyundi@users.noreply.github.com>
2024-04-13 01:51:19 +0800

    Severe bug fixed

--- save_header_to_txt.py
+++ save_header_to_txt.py
@@ -1,16 +1,8 @@
 from astropy.io import fits
 
-outfile = open('header_.txt', 'w')
+outfile = 'header.txt'
 
 hdulist = fits.open('spec1d.m46.034.slit.fits')
 hdr01   = hdulist[1].header
-outfile.write(hdr01)
-outfile.close()
-
-infile = open('header_.txt',  'r')
-oufile = open('header.txt', 'w')
-for line in infile:
-    for j in range(len(line)//80 - 1):
-        oufile.write(line[j*80:(j+1)*80]+'\n')
-infile.close()
-oufile.close()
+hdr01.tofile(outfile, sep='\n', endcard=False,
+             padding=False, overwrite=True)
