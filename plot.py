31e96ea1d3425e21ab5d5d2f3bd9a254db8e7b70
Jiyun Di <76240530+jiyundi@users.noreply.github.com>
2024-04-13 01:20:16 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
-
+plt.style.use('classic')
+plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
+# plt.rcParams.update({
+#         "font.family": "sans-serif",
+#         "font.sans-serif": "Helvetica",
+#         "font.serif": "Helvetica",
+#     })
+# matplotlib.rcParams['mathtext.fontset'] = 'custom'
+# matplotlib.rcParams['mathtext.rm'] = 'Helvetica'
+# matplotlib.rcParams['mathtext.it'] = 'Helvetica:italic'
+    
 fig = plt.figure(figsize=(7,9),dpi=100) # height=7
 plt.subplots_adjust(hspace=0.15, wspace=0.20) # h=height
 gs = fig.add_gridspec(2, 1,
